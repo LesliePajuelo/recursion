80c146255401dcae6839ea8f32fbe1852ddf2af6
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-10-05 15:24:52 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,51 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+    
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return "null";
+  } 
+
+  if (typeof obj === 'boolean'){
+    if (obj === true) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  var holder = [];
+    if(Array.isArray(obj)){
+      
+        for(var key in obj){
+            holder.push(stringifyJSON(obj[key]));
+        }
+      
+      return "[" + holder + "]";   
+
+  }
+
+  
+  var stringContainer = ""; 
+  if (Object.keys(obj).length){
+    for(var keys in obj) {
+      if(typeof obj[keys] === "function" || typeof obj[keys] === "undefined"){
+        continue;
+      } 
+      stringContainer += '"' + keys + '"' + ':' + stringifyJSON(obj[keys]) + ',';
+
+    }
+    return '{' + stringContainer.slice(0,-1) + '}';   
+  } else {
+    return "{}";
+  }
+  
+  };
